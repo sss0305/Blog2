c3a5b724c9986a7038212ea79b94049063c5d4a4
sss0305 <exploremexicoguide@gmail.com>
2022-06-21 16:58:51 -0500

    Third commit

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hi! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern had been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hi! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern had been modified for <a href=\"http://rubyschool.us/\">me</a>"			
 end
